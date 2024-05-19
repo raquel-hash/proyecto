82338f4d5d879763bc952e20acdfca31776be0f4
Raquel <quelcaraquel1@gmail.com>
2024-05-19 18:32:46 -0400

    Eloquent

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,11 +6,11 @@
 
 @foreach( $posts as $post )
 <p>
-	<strong>{{ $post['id'] }}</strong>
-	<a href="{{ route('post', $post['slug']) }}">
-		{{ $post['title'] }}
+	<strong>{{ $post->id }}</strong>
+	<a href="{{ route('post', $post->slug) }}">
+		{{ $post->title }}
 	</a>
 </p>
 @endforeach
-
+    {{ $posts->links() }}
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
 Route::controller(PageController::class)->group(function () {
     Route::get('/',           'home')->name('home');
     Route::get('blog',        'blog')->name('blog');
-    Route::get('blog/{slug}', 'post')->name('post');
+    Route::get('blog/{post:slug}', 'post')->name('post');
 });
