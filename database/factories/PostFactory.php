3502682ab6dd18b35104c63edd28a024a9167840
Raquel <quelcaraquel1@gmail.com>
2024-05-19 17:01:03 -0400

    Models

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $title = $this->faker->sentence(),
+            'slug' => Str::slug($title),
+            'body' => $this->faker->text(2200),
+        ];
+    }
+}
