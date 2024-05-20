c45a5ce962baf7e0809cf44cfa6d9c218d6d558a
Raquel <quelcaraquel1@gmail.com>
2024-05-20 09:23:35 -0400

    Inicio de sesión

--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,16 @@
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-/**
- * Route::get  | Consultar
- * Route::post  | Guardar
- * Route::delete  | Eliminar
- * Route::put  | Actualizar
- */
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 /**
 Route::get('/', [PageController::class, 'home'])->name('home');
 
@@ -18,7 +22,13 @@ Route::get('blog/{slug}', [PageController::class, 'post'])->name('post');
 */
 
 Route::controller(PageController::class)->group(function () {
-    Route::get('/',           'home')->name('home');
-    Route::get('blog',        'blog')->name('blog');
+    Route::get('/',                'home')->name('home');
+    Route::get('blog',             'blog')->name('blog');
     Route::get('blog/{post:slug}', 'post')->name('post');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
