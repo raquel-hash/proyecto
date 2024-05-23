56ed4cd8a64e73224fc39f128a75730618b43501
Raquel <quelcaraquel1@gmail.com>
2024-05-23 16:29:06 -0400

    Crear y editar (primer paso)

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,10 +14,19 @@ class PostController extends Controller
         ]);
     }
 
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function edit(Post $post)
+    {
+        return view('posts.edit', ['post' => $post]);
+    }
+
     public function destroy(Post $post)
     {
         $post->delete();
         return back();
     }
-
 }
