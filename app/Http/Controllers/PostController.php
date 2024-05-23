da79277904dd72d7941aae879898e6851d2a35eb
Raquel <quelcaraquel1@gmail.com>
2024-05-23 16:45:05 -0400

    Funcion de guardar

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class PostController extends Controller
 {
@@ -18,6 +20,17 @@ class PostController extends Controller
     {
         return view('posts.create', ['post' => $post]);
     }
+
+    public function store(Request $request)
+    {
+        $post = $request->user()->posts()->create([
+            'title' => $title = $request->title,
+            'slug' => Str::slug($title),
+            'body' => $request->body,
+        ]);
+        return redirect()->route('posts.edit', $post);
+    }
+
 
     public function edit(Post $post)
     {
